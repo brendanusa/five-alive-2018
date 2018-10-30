bbdde0651d7fd115717af653db50ff54897c8c8b
Brendan Bansavage <brendaniscool@gmail.com>
2018-10-30 01:48:30 -0700

    add prod build

---
+++ client/build/precache-manifest.3d268c08b14d6d90af6b542063ade65c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9de131a82bbd3f931eb9",
+    "url": "/static/css/main.1f0ac03a.chunk.css"
+  },
+  {
+    "revision": "9de131a82bbd3f931eb9",
+    "url": "/static/js/main.9de131a8.chunk.js"
+  },
+  {
+    "revision": "2e247460eb2ab4cb257a",
+    "url": "/static/js/1.2e247460.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1f943add9f15f2427b954ebc360d32ae",
+    "url": "/index.html"
+  }
+];
