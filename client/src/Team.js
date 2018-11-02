66864d1724073364221a066d08cf0daeb852bff7
Brendan Bansavage <brendaniscool@gmail.com>
2018-11-01 19:26:26 -0700

    allow user to view and select teams in teamselect view

---
+++ client/src/Team.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Team.css';
+
+const Team = (props) => {
+
+  return (
+    <div className="TeamSelectRows">
+      <span className="MainTeamName">{props.name}</span>
+      <span className="MainTeamRecord">{props.w}-{props.l}</span>
+    </div>
+  )
+
+}
+
+export default Team;
