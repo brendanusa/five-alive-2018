1a9638c312df6cef29b50f5344d6635988b98c1c
Brendan Bansavage <brendaniscool@gmail.com>
2018-11-06 23:47:07 -0800

    add chat component with connection to socket server

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Chat.css';
 import Message from './Message';
-// import { subscribeToMessages } from './api';
+import io from "socket.io-client";
 
 class Chat extends Component {
 
@@ -12,20 +12,21 @@ class Chat extends Component {
       post: '',
       user: {},
       messages: [],
-      temp: 'null'
     }
-    // subscribeToMessages((err, messages) => this.setState({
-    //   temp: messages
-    // }))
-  }
+    this.socket = io('localhost:8000');
 
-  componentDidMount() {
+    var context = this;
 
-    const validCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A',
-    'B','C',
-    'D',
-    'E',
-    'F',
+    this.socket.on('RECEIVE_MESSAGE', function(data){
+      fetch('/api/messages')
+        .then(res => res.json())
+        .then(data => {
+          context.setState({messages: data})
+        })
+    });
+
+    this.validCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A',
+    'B','C','D','E','F',
     'G',
     'H',
     'I',
@@ -47,23 +48,29 @@ class Chat extends Component {
     'Y',
     'Z','!','?','-','+','1','2','3','4','5','6','7','8','9','0'];
 
-    const checkChars = message => {
+  }
+
+  componentDidMount() {
+
+
+
+    fetch('/api/messages')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({messages: data})
+      })
+  }
+
+    checkChars = message => {
       // let isMessageValid = true;
       for (let i = 0; i < message.length; i++) {
-        if (!validCharacters.includes(message[i])) {
+        if (!this.validCharacters.includes(message[i])) {
+          console.log('message[i]', message[i])
           return false;
         }
       }
       return true;
     }
-
-    // fetch('/api/messages')
-    //   .then(res => res.json())
-    //   .then(data => {
-    //     this.setState({messages: data})
-    //   })
-  }
-
 
   handlePasswordSubmit = e => {
     e.preventDefault();
@@ -75,11 +82,42 @@ class Chat extends Component {
           if (data[0]) {
             console.log(data[0]);
             this.setState({user: data[0]});
-            this.setState({authenticated: true});
+            this.setState({isAuthenticated: true});
+            this.setState({post: ''})
           } else {
             window.alert('Invalid password!')
           }
         })
+
+    // const ChatFooter = () => {
+
+    //   if (!this.state.isAuthenticated) {
+    //     return (
+    //       <form onSubmit={this.handlePasswordSubmit}>
+    //               <p>Enter password:</p>
+    //               <input
+    //                 type="text"
+    //                 value={this.state.post}
+    //                 onChange={e => this.setState({post: e.target.value})}
+    //               />
+    //               <button type="submit" style={{marginLeft:"10px"}}>Submit</button>
+    //             </form>
+    //     )
+    //   } else {
+    //     return (
+    //       <form onSubmit={this.handleMessageSubmit}>
+    //               <p>{this.state.user.name}:</p>
+    //               <input
+    //                 type="text"
+    //                 value={this.state.post}
+    //                 onChange={e => this.setState({post: e.target.value})}
+    //               />
+    //               <button type="submit" style={{marginLeft:"10px"}}>Send!</button>
+    //             </form>
+    //     )
+    //   }
+
+    // }
   }
 
 
@@ -87,60 +125,38 @@ class Chat extends Component {
     e.preventDefault();
     console.log(this.state.post)
     if (this.state.post.length === 0 || this.state.post.length > 200) {
-      return window.alert('Max 200 characters!');
+      return window.alert('Please enter between 1 and 200 characters!');
     }
-    if (this.checkChars(!this.state.post)) {
+    if (!this.checkChars(this.state.post)) {
       return window.alert('Letters, numbers, !, ?, +, and - only! No fancy stuff.');
     }
-    fetch('/api/message?message=' + this.state.post)
+    fetch(`/api/message?userid=${this.state.user.id}&username=${this.state.user.name}&text=${this.state.post}`)
       .then(res => res.json())
       .then(data => {
-        console.log(data[0])
-        if (data[0]) {
-          console.log(data[0]);
-          this.setState({user: data[0]});
-          this.setState({authenticated: true});
-          this.setState({passwordFeedback: `Welcome ${this.state.user.name.split(' ')[0]}!`});
+        console.log(data)
+        if (data) {
+          this.socket.emit('SEND_MESSAGE', {
+              author: this.state.user.name,
+              message: this.state.post
+          });
+          this.setState({post: ''});
         } else {
           window.alert('Message failed!')
         }
       })
   }
 
-  ChatFooter = () => {
-
-    if (!this.state.isAuthenticated) {
-      return (
-        <form onSubmit={this.handlePasswordSubmit}>
-                <p>Enter password:</p>
-                <input
-                  type="text"
-                  value={this.state.post}
-                  onChange={e => this.setState({post: e.target.value})}
-                />
-                <button type="submit" style={{marginLeft:"10px"}}>Submit</button>
-              </form>
-      )
+  handleSubmit = (e) => {
+    if (this.state.isAuthenticated) {
+      this.handleMessageSubmit(e);
     } else {
-      return (
-        <form onSubmit={this.handleMessageSubmit}>
-                <p>{this.state.user.name}:</p>
-                <input
-                  type="text"
-                  value={this.state.post}
-                  onChange={e => this.setState({post: e.target.value})}
-                />
-                <button type="submit" style={{marginLeft:"10px"}}>Send!</button>
-              </form>
-      )
+      this.handlePasswordSubmit(e);
     }
-
   }
 
   render() {
     return (
       <div className="Chat">
-        {this.state.temp}
         <div className="Messages">
           {this.state.messages.map((message, i) => {
             return (
@@ -149,6 +165,16 @@ class Chat extends Component {
           })}
         </div>
         <div className="ChatFooter">
+          <form onSubmit={this.handleSubmit}>
+            <span>{this.state.isAuthenticated ? `Posting as ${this.state.user.name}! ` : 'Enter password to post messages-->'}</span>
+            <input
+              type="text"
+              value={this.state.post}
+              onChange={e => this.setState({post: e.target.value})}
+              style={{width:"400px"}}
+            />
+            <button type="submit" style={{marginLeft:"10px"}}>{this.state.isAuthenticated ? 'Send' : 'Submit'}</button>
+          </form>
         </div>
       </div>
     )
