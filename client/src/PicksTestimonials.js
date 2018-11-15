a8a77f11add7be1daa89b5577bb778d5ea5c2bd1
Brendan Bansavage <brendaniscool@gmail.com>
2018-11-14 21:13:31 -0800

    complete picks page testimonials

---
+++ client/src/PicksTestimonials.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './PicksTestimonials.css';
+
+const PicksTestimonials = () => {
+
+  return (
+    <div className="PicksTestimonials">
+          <div className="PicksTestimonialsHeader">
+            <h2>PICKS PAGE TESTIMONIALS</h2>
+          </div>
+          <div className="PicksTestimonialsContainer">
+              <div className="PicksTestimonialLeft">
+                <div className="PicksTestimonialQuote">
+                "I was looking for info on everyone's picks one evening, and the Picks page had everything I wanted and more."
+                </div>
+                <div className="PicksTestimonialAuthor">
+                --2018 #5ALIVE Runner-up Coreen Kopper
+                </div>
+              </div>
+              <div className="PicksTestimonialRight">
+                <div className="PicksTestimonialQuote">
+                "[mind blown emoji]"
+                </div>
+                <div className="PicksTestimonialAuthor">
+                --2018 #5ALIVE Participant Patrick Godfrey
+                </div>
+              </div>
+              <div className="PicksTestimonialLeft">
+                <div className="PicksTestimonialQuote">
+                "I met my mistress on the #5ALIVE Picks page - we've been sneaking around and committing lots of adultery ever since"
+                </div>
+                <div className="PicksTestimonialAuthor">
+                --Steve Johnson
+                </div>
+              </div>
+              <div className="PicksTestimonialRight">
+                <div className="PicksTestimonialQuote">
+                "The #5ALIVE analytics and research team has aboslutely outdone themselves this time with the 2018 Picks page. It has a list of everyone's teams, plus two other lists for added lists. God bless you, #5ALIVE!"
+                </div>
+                <div className="PicksTestimonialAuthor">
+                --Four-time NBA MVP Wilt Chamberlain
+                </div>
+              </div>
+          </div>
+        </div>
+  )
+
+}
+
+export default PicksTestimonials;
