ed277172595f7c1ad3ba271aed231577b71b0cea
Brendan Bansavage <brendaniscool@gmail.com>
2018-12-21 15:44:26 -0500

    reformat testimonials components for mobile

--- client/src/ChatTestimonials.js
+++ client/src/ChatTestimonials.js
@@ -1,48 +1,48 @@
 import React from 'react';
-import './ChatTestimonials.css';
+import './Testimonials.css';
 
 const ChatTestimonials = () => {
 
   return (
-    <div className="ChatTestimonials">
-          <div className="ChatTestimonialsHeader">
-            <h2>CHAT TESTIMONIALS</h2>
+    <div>
+      <div className="TestimonialsHeader">
+        <h2>CHAT TESTIMONIALS</h2>
+      </div>
+      <div className="TestimonialsBody">
+        <div className="TestimonialLeft">
+          <div className="TestimonialQuote">
+          "I was feeling chatty one afternoon, and it turned out the #5ALIVE was exactly what I was looking for."
           </div>
-          <div className="ChatTestimonialsContainer">
-              <div className="ChatTestimonialLeft">
-                <div className="ChatTestimonialQuote">
-                "I was feeling chatty one afternoon, and it turned out the #5ALIVE chat was exactly what I was looking for."
-                </div>
-                <div className="ChatTestimonialAuthor">
-                --2018 #5ALIVE Runner-up Coreen Kopper
-                </div>
-              </div>
-              <div className="ChatTestimonialRight">
-                <div className="ChatTestimonialQuote">
-                "Very impressive!"
-                </div>
-                <div className="ChatTestimonialAuthor">
-                --2014 #5ALIVE Champion Tim Falkner
-                </div>
-              </div>
-              <div className="ChatTestimonialLeft">
-                <div className="ChatTestimonialQuote">
-                "I met my fiancee on the #5ALIVE chat - we've been inseparable ever since"
-                </div>
-                <div className="ChatTestimonialAuthor">
-                --Don Jepsen
-                </div>
-              </div>
-              <div className="ChatTestimonialRight">
-                <div className="ChatTestimonialQuote">
-                "The #5ALIVE chat is a great way to stay in touch with friends and family, and the best part is it's built on dynamic #5ALIVE chat technology!"
-                </div>
-                <div className="ChatTestimonialAuthor">
-                --Seven-time NBA Scoring Champion Wilt Chamberlain
-                </div>
-              </div>
+          <div className="TestimonialAuthor">
+          --2018 #5ALIVE Runner-up Coreen Kopper
           </div>
         </div>
+        <div className="TestimonialRight">
+          <div className="TestimonialQuote">
+          "Very impressive!"
+          </div>
+          <div className="TestimonialAuthor">
+          --2014 #5ALIVE Champion Tim Falkner
+          </div>
+        </div>
+        <div className="TestimonialLeft">
+          <div className="TestimonialQuote">
+          "I met my fiancee on the #5ALIVE chat - we've been inseparable ever since"
+          </div>
+          <div className="TestimonialAuthor">
+          --Don Jepsen
+          </div>
+        </div>
+        <div className="TestimonialRight">
+          <div className="TestimonialQuote">
+          "The #5ALIVE chat is a great way to stay in touch with friends and family, and the best part is it's built on dynamic #5ALIVE chat technology!"
+          </div>
+          <div className="TestimonialAuthor">
+          --Seven-time NBA Scoring Champion Wilt Chamberlain
+          </div>
+        </div>
+      </div>
+    </div>
   )
 
 }
--- client/src/PicksTestimonials.js
+++ client/src/PicksTestimonials.js
@@ -1,48 +1,51 @@
 import React from 'react';
-import './PicksTestimonials.css';
+import './Testimonials.css';
 
 const PicksTestimonials = () => {
 
+  console.log('HELLO')
+
   return (
-    <div className="PicksTestimonials">
-          <div className="PicksTestimonialsHeader">
-            <h2>PICKS PAGE TESTIMONIALS</h2>
+
+    <div>
+      <div className="TestimonialsHeader">
+        <h2>PICKS PAGE TESTIMONIALS</h2>
+      </div>
+      <div className="TestimonialsBody">
+        <div className="TestimonialLeft">
+          <div className="TestimonialQuote">
+          "I was looking for info on everyone's picks one evening, and the #5ALIVE Picks page had everything I wanted and more."
           </div>
-          <div className="PicksTestimonialsContainer">
-              <div className="PicksTestimonialLeft">
-                <div className="PicksTestimonialQuote">
-                "I was looking for info on everyone's picks one evening, and the #5ALIVE Picks page had everything I wanted and more."
-                </div>
-                <div className="PicksTestimonialAuthor">
-                --2018 #5ALIVE Runner-up Coreen Kopper
-                </div>
-              </div>
-              <div className="PicksTestimonialRight">
-                <div className="PicksTestimonialQuote">
-                "[mind blown emoji]"
-                </div>
-                <div className="PicksTestimonialAuthor">
-                --2018-19 #5ALIVE Participant Patrick Godfrey
-                </div>
-              </div>
-              <div className="PicksTestimonialLeft">
-                <div className="PicksTestimonialQuote">
-                "I met my mistress on the #5ALIVE Picks page - we've been sneaking around and committing lots of adultery ever since"
-                </div>
-                <div className="PicksTestimonialAuthor">
-                --Steve Johnson
-                </div>
-              </div>
-              <div className="PicksTestimonialRight">
-                <div className="PicksTestimonialQuote">
-                "The #5ALIVE analytics and research team has aboslutely outdone themselves this time with the 2018 Picks page. It has a list of everyone's teams, plus two other lists for added lists. God bless you, #5ALIVE!"
-                </div>
-                <div className="PicksTestimonialAuthor">
-                --Four-time NBA MVP Wilt Chamberlain
-                </div>
-              </div>
+          <div className="TestimonialAuthor">
+          --2018 #5ALIVE Runner-up Coreen Kopper
           </div>
         </div>
+        <div className="TestimonialRight">
+          <div className="TestimonialQuote">
+          "[mind blown emoji]"
+          </div>
+          <div className="TestimonialAuthor">
+          --2018-19 #5ALIVE Participant Patrick Godfrey
+          </div>
+        </div>
+        <div className="TestimonialLeft">
+          <div className="TestimonialQuote">
+          "I met my mistress on the #5ALIVE Picks page - we've been sneaking around and committing lots of adultery ever since"
+          </div>
+          <div className="TestimonialAuthor">
+          --Steve Johnson
+          </div>
+        </div>
+        <div className="TestimonialRight">
+          <div className="TestimonialQuote">
+          "The #5ALIVE analytics and research team has aboslutely outdone themselves this time with the 2018 Picks page. It has a list of everyone's teams, plus two other lists for added lists. God bless you, #5ALIVE!"
+          </div>
+          <div className="TestimonialAuthor">
+          --Four-time NBA MVP Wilt Chamberlain
+          </div>
+        </div>
+      </div>
+    </div>
   )
 
 }
