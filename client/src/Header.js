b18981d6345d63831b8035e325b391fbcd19c658
Brendan Bansavage <brendaniscool@gmail.com>
2019-04-15 23:48:53 -0700

    add pick 5 link and highlighting to links in header

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import React, { Component } from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import './Header.css';
         
@@ -18,7 +19,7 @@ const Header = () => (
       <span className="bordered" id="six">V</span>
       <span className="bordered" id="seven">E</span>
     </strong></span>
-    <span className="link">
+    <span className="links">
         <span>
           <Link to={'/home'}>HOME</Link>
         </span>
@@ -31,8 +32,71 @@ const Header = () => (
         <span>
           <Link to={'/about'}>ABOUT</Link>
         </span>
+        <span style={{marginLeft: '1.5vw'}}>
+          <Link to={'/pickfive'}>PICK5</Link>
+        </span>
     </span>
   </header>
 )
+
+// class Header extends Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       activeComponent: 'home'
+//     }
+//   }
+
+//   componentDidMount() {
+
+//   }
+
+//   handleClick = (e) => {
+//     console.log('58', this.state.activeComponent)
+//     if (e.target.href) {
+//       let url = e.target.href.slice(e.target.href.lastIndexOf('/') + 1) || 'home';
+//       console.log('url', url)
+//       document.getElementById(this.state.activeComponent).className -= 'selectedComponent';
+//       this.setState({activeComponent: url})
+//       document.getElementById(url).className += 'selectedComponent';
+//     }
+//     console.log('67', this.state.activeComponent)
+//   }
+
+//   render() {
+//     return(
+//       <header>
+//         <span className="title"><strong>
+//           <span className="bordered" id="one">#</span>
+//           <span className="bordered" id="two">5</span>
+//           <span className="bordered" id="three">A</span>
+//           <span className="bordered" id="four">L</span>
+//           <span className="bordered" id="five">I</span>
+//           <span className="bordered" id="six">V</span>
+//           <span className="bordered" id="seven">E</span>
+//         </strong></span>
+//         <span className="links" onClick={this.handleClick}>
+//             <span>
+//               <Link to={'/home'}>HOME</Link>
+//             </span>
+//             <span>
+//               <Link to={'/stats'}>STATS</Link>
+//             </span>
+//             <span>
+//               <Link to={'/history'}>HISTORY</Link>
+//             </span>
+//             <span>
+//               <Link to={'/about'}>ABOUT</Link>
+//             </span>
+//             <span style={{marginLeft: '1.5vw'}}>
+//               <Link to={'/pickfive'}>PICK5</Link>
+//             </span>
+//         </span>
+//       </header>
+//     )
+//   }
+
+// }
 
 export default Header;
