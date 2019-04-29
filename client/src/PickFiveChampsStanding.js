08e53938a1f28543c98aea236370c85ccc7ff2b4
Brendan Bansavage <brendaniscool@gmail.com>
2019-04-28 23:44:45 -0700

    create and complete pick five standing components

---
+++ client/src/PickFiveChampsStanding.js
@@ -0,0 +1,38 @@
+import './PickFiveChampsStanding.css';
+import React, { Component } from 'react';
+import PickFiveStandingsTeamList from './PickFiveStandingsTeamList';
+
+class PickFiveChampsStanding extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: false
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({isActive: !this.state.isActive})
+  }
+
+  render() {
+    return(
+      <div className="PickFiveChampsStanding" onClick={this.handleClick}>
+        <div className="PickFiveChampsStandingHeader">
+          <div className="PickFiveChampsStandingYear">
+            <span>{this.props.year}</span>
+          </div>
+          <div className="PickFiveChampsStandingName">{this.props.name}</div>
+        </div>
+        <div>
+          {this.state.isActive ? <PickFiveStandingsTeamList teams={this.props.teams} /> : null}
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default PickFiveChampsStanding;
---
+++ client/src/PickFiveStanding.js
@@ -0,0 +1,36 @@
+import './PickFiveStanding.css';
+import React, { Component } from 'react';
+import PickFiveStandingsTeamList from './PickFiveStandingsTeamList';
+
+class PickFiveStanding extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: false
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({isActive: !this.state.isActive})
+  }
+
+  render() {
+    return(
+      <div className="PickFiveStanding" onClick={this.handleClick}>
+        <div className="PickFiveStandingHeader">
+          <span className="PickFiveStandingName">{this.props.name}</span>
+          <span className="PickFiveStandingScore">{this.props.score}</span>
+        </div>
+        <div>
+          {this.state.isActive ? <PickFiveStandingsTeamList teams={this.props.teams} /> : null}
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default PickFiveStanding;
