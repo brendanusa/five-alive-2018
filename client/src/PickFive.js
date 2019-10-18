3dd1552a3de84c5463cab5e8cd50e2b769496a87
Brendan Bansavage <brendaniscool@gmail.com>
2019-10-17 21:34:48 -0700

    move pick5 components to slick carousel

--- client/src/PickFive.js
+++ client/src/PickFive.js
@@ -3,6 +3,9 @@ import './PickFive.css';
 import PickFiveStandings from './PickFiveStandings';
 import PickFiveChamps from './PickFiveChamps';
 import PickFiveHighScores from './PickFiveHighScores';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 class PickFive extends Component {
 
@@ -18,6 +21,17 @@ class PickFive extends Component {
   }
 
   render() {
+    var settings = {
+      centerMode: true,
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      variableWidth: true,
+      initialSlide: 1,
+      // centerPadding: '100px'
+    };
     return (
       <div>
         <div id="dim">
@@ -25,9 +39,11 @@ class PickFive extends Component {
           Welcome to the PICK 5, #5ALIVE'S tournament-only cousin. Gaze in awe at the the all-time high scores, or take an inspiring stroll down the Hall of Champions. (click header to expand/collapse)
           </div>
           <div className="PickFive">
-            <div><PickFiveStandings /></div>
-            <div><PickFiveChamps /></div>
-            <div><PickFiveHighScores /></div>
+            <Slider {...settings}>
+              <div><PickFiveStandings /></div>
+              <div><PickFiveChamps /></div>
+              <div><PickFiveHighScores /></div>
+            </Slider>
           </div>
         </div>
       </div>
