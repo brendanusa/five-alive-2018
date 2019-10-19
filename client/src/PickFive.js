9be763734c253b7d2ab465366b7efd16abb67121
Brendan Bansavage <brendaniscool@gmail.com>
2019-10-18 17:05:23 -0700

    wrap and center carousel around slides

--- client/src/PickFive.js
+++ client/src/PickFive.js
@@ -38,7 +38,7 @@ class PickFive extends Component {
           <div className="PickFiveDescription">
           Welcome to the PICK 5, #5ALIVE'S tournament-only cousin. Gaze in awe at the the all-time high scores, or take an inspiring stroll down the Hall of Champions. (click header to expand/collapse)
           </div>
-          <div className="PickFive">
+          <div className="PickFive" style={{height:'100%'}}>
             <Slider {...settings}>
               <div><PickFiveStandings /></div>
               <div><PickFiveChamps /></div>
