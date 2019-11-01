44b1771550858fed155e1802f2f358fa20dbc353
Brendan Bansavage <brendaniscool@gmail.com>
2019-10-31 19:31:57 -0700

    complete pick5 components

--- client/src/PickFiveStandings.js
+++ client/src/PickFiveStandings.js
@@ -2,44 +2,12 @@ import React, { Component } from 'react';
 import './PickFiveStandings.css';
 import PickFiveStanding from './PickFiveStanding';
 
-// const PickFiveStandingHard = (props) => {
-//   return (
-//     <div className="PickFiveStandingHard">
-//       <span>{props.name}</span>
-//       <span>{props.wins}</span>
-//     </div>
-//   )
-// }
-
 class PickFiveStandings extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      standingsHard: {
-        'Dave Andrist': 149,
-        'Chris Godfrey': 110,
-        'T. French/S. Reiman': 110,
-        'Brendan Bansavage': 104,
-        'Chris Jennings': 87,
-        'Nick Kordesch': 86,
-        'Gil Estupinan': 85,
-        'Zach Baron': 65,
-        'Patrick Godfrey': 54,
-        'Toby Perry': 54,
-        'Dan Vogel': 51,
-        'Noah Garber': 50,
-        'Alex Raup': 48,
-        'Coreen Kopper': 45,
-        'Krysti Walker': 39,
-        'Matt Gordon': 32,
-        'Casey Schutte': 25,
-        'Andrew Soltish': 22,
-        'Reed Lyon': 19,
-        'Dillon Smith': 18,
-        'Patrick Cushing': 18,
-        'Anish Kurian': 12,
-      },
+      standings: [],
       isActive: false
     }
 
@@ -48,7 +16,13 @@ class PickFiveStandings extends Component {
   }
 
   handleClick() {
-    this.setState({isActive: !this.state.isActive})
+    this.setState({isActive: !this.state.isActive});
+    fetch('api/pickfive/standings')
+      .then(res => res.json())
+      .then(data => {
+        console.log('data', data)
+        this.setState({standings: data});
+      })
   }
 
   render() {
@@ -60,8 +34,8 @@ class PickFiveStandings extends Component {
 
         <div className="PickFiveStandings">
           <div>
-          {Object.keys(this.state.standingsHard).map((person, i) => {
-            return (<PickFiveStanding key={i} name={person} score={this.state.standingsHard[person]}/>)
+          {this.state.standings.map((person, i) => {
+            return (<PickFiveStanding key={i} name={person.name} score={person.score} teams={person.teams}/>)
           })}
           </div>
         </div>
