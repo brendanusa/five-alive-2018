14562b1bafa752d4818abaa5ab01a18a1557ca69
Brendan Bansavage <brendaniscool@gmail.com>
2020-11-20 15:06:54 -0800

    remove scoreboard import from server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 const pgp = require('pg-promise')(/*options*/);
 const collectTeamData = require('./collectTeamData');
 const pickFiveData = require('./pickFiveData');
-const scoreboard = require('./scoreboard');
+// const scoreboard = require('./scoreboard');
 const db = pgp(process.env.DATABASE_URL || 'postgres://akppnbjeltipma:d83a3e7a826cd09a205551a1e4063b60f365201ca4ad6ed875dfdc5cb4e07bac@ec2-54-243-46-32.compute-1.amazonaws.com:5432/d35h8248bl7gm9?ssl=true');
 
 // DYNAMIC DB URL
