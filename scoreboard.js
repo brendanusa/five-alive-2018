c2b1a6c1221d3698d245965363ad900c6e5c6f9d
Brendan Bansavage <brendaniscool@gmail.com>
2021-01-01 13:29:35 -0800

    apply temp fix for single digit month+day in scraper

--- scoreboard.js
+++ scoreboard.js
@@ -11,7 +11,7 @@ const fetchScores = (db) => {
       })
       // parse current date and insert into url
       var date = new Date(Date.now())
-      axios.get(`http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/scoreboard?limit=500&dates=${date.getFullYear()}${date.getMonth()+1}${date.getDate()}&groups=50`)
+      axios.get(`http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/scoreboard?limit=500&dates=${date.getFullYear()}0${date.getMonth()+1}0${date.getDate()}&groups=50`)
         .then(res => {
           for (i = 0; i < res.data.events.length; i++) {
             // check for active team
@@ -49,5 +49,5 @@ setTimeout(function() {
   fetchScores(pgp(process.env.DATABASE_URL || 'postgres://akppnbjeltipma:d83a3e7a826cd09a205551a1e4063b60f365201ca4ad6ed875dfdc5cb4e07bac@ec2-54-243-46-32.compute-1.amazonaws.com:5432/d35h8248bl7gm9?ssl=true'));
 }, Math.random()*60000)
 
-//immediate call(for debugging)
+// immediate call for debugging
 // fetchScores(pgp(process.env.DATABASE_URL || 'postgres://akppnbjeltipma:d83a3e7a826cd09a205551a1e4063b60f365201ca4ad6ed875dfdc5cb4e07bac@ec2-54-243-46-32.compute-1.amazonaws.com:5432/d35h8248bl7gm9?ssl=true'))
