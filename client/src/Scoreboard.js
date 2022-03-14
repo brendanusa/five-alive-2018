e90811182fbf9a61fe99c06a91d1b6b8b66f5137
Brendan Bansavage <brendaniscool@gmail.com>
2022-03-14 15:38:36 -0700

    add date to scoreboard

--- client/src/Scoreboard.js
+++ client/src/Scoreboard.js
@@ -1,40 +1,53 @@
-import './Scoreboard.css';
-import React, { Component } from 'react';
-import ScoreboardGame from './ScoreboardGame';
+import "./Scoreboard.css";
+import React, { Component } from "react";
+import ScoreboardGame from "./ScoreboardGame";
 
 class Scoreboard extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      scores: []
-    }
+      scores: [],
+    };
   }
 
   componentDidMount() {
-    fetch('/api/scores')
-      .then(res => res.json())
-      .then(data => {
-        this.setState({scores: data})
-      })
+    fetch("/api/scores")
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ scores: data });
+      });
   }
 
   render() {
-    return(
+    return (
       <div className="Scoreboard">
-        {this.state.scores[0] ?
-          this.state.scores.map((game, i) => {
-            return (
-              <ScoreboardGame key={i} homeTeam={game.hometeam} homeScore={game.homescore} awayTeam={game.awayteam} awayScore={game.awayscore} clock={game.clock} id={game.id}/>
-            )
-          }) :
-          <div className="Empty">NO #5ALIVE GAMES TODAY. FEEL FREE TO GO OUTSIDE OR INTERACT WITH LOVED ONES.</div>
-        }
+        <div className="ScoreboardDate">MARCH 13</div>
+        <div className="ScoreboardGamesContainer">
+          {this.state.scores[0] ? (
+            this.state.scores.map((game, i) => {
+              return (
+                <ScoreboardGame
+                  key={i}
+                  homeTeam={game.hometeam}
+                  homeScore={game.homescore}
+                  awayTeam={game.awayteam}
+                  awayScore={game.awayscore}
+                  clock={game.clock}
+                  id={game.id}
+                />
+              );
+            })
+          ) : (
+            <div className="Empty">
+              NO #5ALIVE GAMES TODAY. FEEL FREE TO GO OUTSIDE OR INTERACT WITH
+              LOVED ONES.
+            </div>
+          )}
+        </div>
         <div className="MarginCorrection"></div>
       </div>
     );
   }
-
 }
 
 export default Scoreboard;
