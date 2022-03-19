37e7367642f1f5bf8dfc47d4e70abe8b9804f20e
Brendan Bansavage <brendaniscool@gmail.com>
2022-03-19 11:54:18 -0700

    rmv text from pick5 data

--- pickFiveData.js
+++ pickFiveData.js
@@ -549,380 +549,139 @@ const pickFiveChamps = [
 ];
 
 const pickFiveTeams = [
-  {name: '1) Arizona', points: 1, round: 'Second Rd'}, 
-  {name: '1) Baylor', points: 1, round: 'Second Rd'}, 
-  {name: '1) Gonzaga', points: 1, round: 'Second Rd'}, 
-  {name: '1) Kansas', points: 1, round: 'Second Rd'}, 
-  {name: '10) Davidson', points: 0, round: 'First Rd'}, 
-  {name: '10) Loyola (IL)', points: 0, round: 'First Rd'}, 
-  {name: '10) Miami (FL)', points: 10, round: 'Second Rd'}, 
-  {name: '10) San Francisco', points: 0, round: 'First Rd'}, 
-  {name: '11) Iowa State', points: 11, round: 'Second Rd'}, 
-  {name: '11) Michigan', points: 11, round: 'Second Rd'}, 
-  {name: '11) Virginia Tech', points: 0, round: 'First Rd'}, 
-  {name: '12) Indiana', points: 0, round: 'First Rd'}, 
-  {name: '12) New Mexico State', points: 12, round: 'Second Rd'}, 
-  {name: '12) Richmond', points: 12, round: 'Second Rd'}, 
-  {name: '12) UAB', points: 0, round: 'First Rd'}, 
-  {name: '13) South Dakota State', points: 0, round: 'First Rd'}, 
-  {name: '13) UT-Chattanooga', points: 0, round: 'First Rd'}, 
-  {name: '13) Vermont', points: 0, round: 'First Rd'}, 
-  {name: '14) Colgate', points: 0, round: 'First Rd'}, 
-  {name: '2) Auburn', points: 2, round: 'Second Rd'}, 
-  {name: '2) Duke', points: 2, round: 'Second Rd'}, 
-  {name: '2) Kentucky', points: 0, round: 'First Rd'}, 
-  {name: '2) Villanova', points: 2, round: 'Second Rd'}, 
-  {name: '3) Purdue', points: 3, round: 'Second Rd'}, 
-  {name: '3) Tennessee', points: 3, round: 'Second Rd'}, 
-  {name: '3) Texas Tech', points: 3, round: 'Second Rd'}, 
-  {name: '3) Wisconsin', points: 3, round: 'Second Rd'}, 
-  {name: '4) Arkansas', points: 4, round: 'Second Rd'}, 
-  {name: '4) Illinois', points: 4, round: 'Second Rd'}, 
-  {name: '4) UCLA', points: 4, round: 'Second Rd'}, 
-  {name: '5) Connecticut', points: 0, round: 'First Rd'}, 
-  {name: '5) Houston', points: 5, round: 'Second Rd'}, 
-  {name: '5) Iowa', points: 0, round: 'First Rd'}, 
-  {name: '5) St. Mary\'s', points: 5, round: 'Second Rd'}, 
-  {name: '5) Virginia Tech', points: 0, round: 'First Rd'}, 
-  {name: '6) Alabama', points: 6, round: 'Second Rd'}, 
-  {name: '6) Colorado State', points: 0, round: 'First Rd'}, 
-  {name: '6) LSU', points: 0, round: 'First Rd'}, 
-  {name: '6) Texas', points: 6, round: 'Second Rd'}, 
-  {name: '7) Michigan State', points: 7, round: 'Second Rd'}, 
-  {name: '7) Murray State', points: 0, round: 'First Rd'}, 
-  {name: '7) Ohio State', points: 7, round: 'Second Rd'}, 
-  {name: '7) USC', points: 0, round: 'First Rd'}, 
-  {name: '8) Boise State', points: 0, round: 'First Rd'}, 
-  {name: '8) Memphis', points: 8, round: 'Second Rd'}, 
-  {name: '8) North Carolina', points: 8, round: 'Second Rd'}, 
-  {name: '8) San Diego State', points: 0, round: 'First Rd'}, 
-  {name: '9) Creighton', points: 9, round: 'Second Rd'}, 
+  { name: "1) Arizona", points: 1, round: "Second Rd" },
+  { name: "1) Baylor", points: 1, round: "Second Rd" },
+  { name: "1) Gonzaga", points: 1, round: "Second Rd" },
+  { name: "1) Kansas", points: 1, round: "Second Rd" },
+  { name: "10) Davidson", points: 0, round: "First Rd" },
+  { name: "10) Loyola (IL)", points: 0, round: "First Rd" },
+  { name: "10) Miami (FL)", points: 10, round: "Second Rd" },
+  { name: "10) San Francisco", points: 0, round: "First Rd" },
+  { name: "11) Iowa State", points: 11, round: "Second Rd" },
+  { name: "11) Michigan", points: 11, round: "Second Rd" },
+  { name: "11) Virginia Tech", points: 0, round: "First Rd" },
+  { name: "12) Indiana", points: 0, round: "First Rd" },
+  { name: "12) New Mexico State", points: 12, round: "Second Rd" },
+  { name: "12) Richmond", points: 12, round: "Second Rd" },
+  { name: "12) UAB", points: 0, round: "First Rd" },
+  { name: "13) South Dakota State", points: 0, round: "First Rd" },
+  { name: "13) UT-Chattanooga", points: 0, round: "First Rd" },
+  { name: "13) Vermont", points: 0, round: "First Rd" },
+  { name: "14) Colgate", points: 0, round: "First Rd" },
+  { name: "2) Auburn", points: 2, round: "Second Rd" },
+  { name: "2) Duke", points: 2, round: "Second Rd" },
+  { name: "2) Kentucky", points: 0, round: "First Rd" },
+  { name: "2) Villanova", points: 2, round: "Second Rd" },
+  { name: "3) Purdue", points: 3, round: "Second Rd" },
+  { name: "3) Tennessee", points: 3, round: "Second Rd" },
+  { name: "3) Texas Tech", points: 3, round: "Second Rd" },
+  { name: "3) Wisconsin", points: 3, round: "Second Rd" },
+  { name: "4) Arkansas", points: 4, round: "Second Rd" },
+  { name: "4) Illinois", points: 4, round: "Second Rd" },
+  { name: "4) UCLA", points: 4, round: "Second Rd" },
+  { name: "5) Connecticut", points: 0, round: "First Rd" },
+  { name: "5) Houston", points: 5, round: "Second Rd" },
+  { name: "5) Iowa", points: 0, round: "First Rd" },
+  { name: "5) St. Mary's", points: 5, round: "Second Rd" },
+  { name: "5) Virginia Tech", points: 0, round: "First Rd" },
+  { name: "6) Alabama", points: 6, round: "Second Rd" },
+  { name: "6) Colorado State", points: 0, round: "First Rd" },
+  { name: "6) LSU", points: 0, round: "First Rd" },
+  { name: "6) Texas", points: 6, round: "Second Rd" },
+  { name: "7) Michigan State", points: 7, round: "Second Rd" },
+  { name: "7) Murray State", points: 0, round: "First Rd" },
+  { name: "7) Ohio State", points: 7, round: "Second Rd" },
+  { name: "7) USC", points: 0, round: "First Rd" },
+  { name: "8) Boise State", points: 0, round: "First Rd" },
+  { name: "8) Memphis", points: 8, round: "Second Rd" },
+  { name: "8) North Carolina", points: 8, round: "Second Rd" },
+  { name: "8) San Diego State", points: 0, round: "First Rd" },
+  { name: "9) Creighton", points: 9, round: "Second Rd" },
 ];
 
 const pickFivePicks = [
+  { name: "Ben Aparo", score: 23, teams: [21, 24, 29, 31, 9] },
+  { name: "Brendan Bansavage", score: 6, teams: [22, 29, 32, 10, 17] },
 
-  { name: 'Ben Aparo', score: 23, teams: [21,24,29,31,9]},
-  { name: 'Brendan Bansavage', score: 6, teams: [22,29,32,10,17]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Zach Baron', score: 9, teams: [21,29,31,32,43]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Sam Bazar', score: 6, teams: [24,25,32,5,16]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Elya Beer', score: 14, teams: [39,40,41,46,15]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Dante Carnevale', score: 7, teams: [24,29,32,4,10]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Edward Carroll', score: 9, teams: [2,22,25,26,36]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Alex Chen', score: 22, teams: [1,22,26,28,12]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Phil Cohen', score: 6, teams: [0,3,29,32,10]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Andrew Collins', score: 16, teams: [26,30,31,32,45]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Ryan Corbalis', score: 14, teams: [22,23,47,4,18]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Patrick Cushing', score: 11, teams: [23,24,30,31,32]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Troy Davis', score: 27, teams: [25,29,45,10,12]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Brendon Desrochers', score: 9, teams: [21,29,31,32,37]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Chris Donaghy', score: 13, teams: [24,31,32,33,40]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Noah Garber', score: 10, teams: [2,22,25,29,10]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Chris Godfrey', score: 12, teams: [0,29,32,39,42]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Harrison Godfrey', score: 24, teams: [3,20,27,41,6]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Patrick Godfrey', score: 14, teams: [39,41,42,7,11]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Kevin Hess', score: 31, teams: [35,38,42,45,9]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Chris Jennings', score: 23, teams: [19,29,32,35,9]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'JP Kelly', score: 25, teams: [24,29,32,35,12]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Nick Kordesch', score: 6, teams: [22,27,40,46,11]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Anish Kurian', score: 11, teams: [24,26,32,33,14]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Kaleigh Lidstone', score: 13, teams: [2,25,29,31,17]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Matt Lidstone', score: 8, teams: [0,25,29,32,10]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Reed Lyon', score: 33, teams: [20,44,9,10,13]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Mark McGill', score: 17, teams: [22,23,28,45,15]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Justin McNamee', score: 11, teams: [21,24,45,14,17]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Casey Schutte', score: 16, teams: [31,32,4,9,10]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Bob Shellenberger', score: 7, teams: [0,19,27,10,17]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Erin Shellenberger', score: 8, teams: [21,22,32,35,17]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Kelly Shellenberger', score: 21, teams: [22,29,33,6,10]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Scott Sherman', score: 9, teams: [0,2,24,29,15]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Adam Sidney', score: 21, teams: [24,27,29,32,6]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Matthew Silva', score: 9, teams: [29,30,31,32,10]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Dillon Smith', score: 4, teams: [28,32,4,11,17]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Edward Stowe', score: 23, teams: [25,29,31,5,9]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Geno Tartell III', score: 6, teams: [0,2,29,32,34]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Dan Vogel', score: 3, teams: [24,4,10,14,15]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Krysti Walker', score: 8, teams: [0,24,29,32,10]},
-  
-  
-  
-  
-  
-  
-  
-  { name: 'Ross Warren', score: 23, teams: [2,32,7,8,9]}
+  { name: "Zach Baron", score: 9, teams: [21, 29, 31, 32, 43] },
+
+  { name: "Sam Bazar", score: 6, teams: [24, 25, 32, 5, 16] },
+
+  { name: "Elya Beer", score: 14, teams: [39, 40, 41, 46, 15] },
+
+  { name: "Dante Carnevale", score: 7, teams: [24, 29, 32, 4, 10] },
+
+  { name: "Edward Carroll", score: 9, teams: [2, 22, 25, 26, 36] },
+
+  { name: "Alex Chen", score: 22, teams: [1, 22, 26, 28, 12] },
+
+  { name: "Phil Cohen", score: 6, teams: [0, 3, 29, 32, 10] },
+
+  { name: "Andrew Collins", score: 16, teams: [26, 30, 31, 32, 45] },
+
+  { name: "Ryan Corbalis", score: 14, teams: [22, 23, 47, 4, 18] },
+
+  { name: "Patrick Cushing", score: 11, teams: [23, 24, 30, 31, 32] },
+
+  { name: "Troy Davis", score: 27, teams: [25, 29, 45, 10, 12] },
+
+  { name: "Brendon Desrochers", score: 9, teams: [21, 29, 31, 32, 37] },
+
+  { name: "Chris Donaghy", score: 13, teams: [24, 31, 32, 33, 40] },
+
+  { name: "Noah Garber", score: 10, teams: [2, 22, 25, 29, 10] },
+
+  { name: "Chris Godfrey", score: 12, teams: [0, 29, 32, 39, 42] },
+
+  { name: "Harrison Godfrey", score: 24, teams: [3, 20, 27, 41, 6] },
+
+  { name: "Patrick Godfrey", score: 14, teams: [39, 41, 42, 7, 11] },
+
+  { name: "Kevin Hess", score: 31, teams: [35, 38, 42, 45, 9] },
+
+  { name: "Chris Jennings", score: 23, teams: [19, 29, 32, 35, 9] },
+
+  { name: "JP Kelly", score: 25, teams: [24, 29, 32, 35, 12] },
+
+  { name: "Nick Kordesch", score: 6, teams: [22, 27, 40, 46, 11] },
+
+  { name: "Anish Kurian", score: 11, teams: [24, 26, 32, 33, 14] },
+
+  { name: "Kaleigh Lidstone", score: 13, teams: [2, 25, 29, 31, 17] },
+
+  { name: "Matt Lidstone", score: 8, teams: [0, 25, 29, 32, 10] },
+
+  { name: "Reed Lyon", score: 33, teams: [20, 44, 9, 10, 13] },
+
+  { name: "Mark McGill", score: 17, teams: [22, 23, 28, 45, 15] },
+
+  { name: "Justin McNamee", score: 11, teams: [21, 24, 45, 14, 17] },
+
+  { name: "Casey Schutte", score: 16, teams: [31, 32, 4, 9, 10] },
+
+  { name: "Bob Shellenberger", score: 7, teams: [0, 19, 27, 10, 17] },
+
+  { name: "Erin Shellenberger", score: 8, teams: [21, 22, 32, 35, 17] },
+
+  { name: "Kelly Shellenberger", score: 21, teams: [22, 29, 33, 6, 10] },
+
+  { name: "Scott Sherman", score: 9, teams: [0, 2, 24, 29, 15] },
+
+  { name: "Adam Sidney", score: 21, teams: [24, 27, 29, 32, 6] },
+
+  { name: "Matthew Silva", score: 9, teams: [29, 30, 31, 32, 10] },
+
+  { name: "Dillon Smith", score: 4, teams: [28, 32, 4, 11, 17] },
+
+  { name: "Edward Stowe", score: 23, teams: [25, 29, 31, 5, 9] },
+
+  { name: "Geno Tartell III", score: 6, teams: [0, 2, 29, 32, 34] },
+
+  { name: "Dan Vogel", score: 3, teams: [24, 4, 10, 14, 15] },
+
+  { name: "Krysti Walker", score: 8, teams: [0, 24, 29, 32, 10] },
+
+  { name: "Ross Warren", score: 23, teams: [2, 32, 7, 8, 9] },
 ];
 
 const parseTeamRound = (pickFiveDataset) => {
@@ -956,297 +715,3 @@ const populateStandingsTeamData = () => {
 module.exports.pickFiveChamps = parseTeamRound(pickFiveChamps);
 module.exports.pickFiveHighScores = parseTeamRound(pickFiveHighScores);
 module.exports.pickFiveStandings = populateStandingsTeamData();
-
-Ben Aparo
-2) Kentucky
-3) Tennessee
-4) UCLA
-5) Houston
-11) Michigan
-
-Brendan Bansavage
-2) Villanova
-4) UCLA
-5) Iowa
-11) Virginia Tech
-13) Vermont
-
-Zach Baron
-2) Kentucky
-4) UCLA
-5) Houston
-5) Iowa
-8) Boise State
-
-Sam Bazar
-3) Tennessee
-3) Texas Tech
-5) Iowa
-10) Loyola (IL)
-13) UT-Chattanooga
-
-Elya Beer
-7) Michigan State
-7) Murray State
-7) Ohio State
-8) San Diego State
-13) South Dakota State
-
-Dante Carnevale
-3) Tennessee
-4) UCLA
-5) Iowa
-10) Davidson
-11) Virginia Tech
-
-Edward Carroll
-1) Gonzaga
-2) Villanova
-3) Texas Tech
-3) Wisconsin
-6) Colorado State
-
-Alex Chen
-1) Baylor
-2) Villanova
-3) Wisconsin
-4) Illinois
-12) New Mexico State
-
-Phil Cohen
-1) Arizona
-1) Kansas
-4) UCLA
-5) Iowa
-11) Virginia Tech
-
-Andrew Collins
-3) Wisconsin
-5) Connecticut
-5) Houston
-5) Iowa
-8) North Carolina
-
-Ryan Corbalis
-2) Villanova
-3) Purdue
-9) Creighton
-10) Davidson
-14) Colgate
-
-Patrick Cushing
-3) Purdue
-3) Wisconsin
-5) Connecticut
-5) Houston
-5) Iowa
-
-Troy Davis
-3) Texas Tech
-4) UCLA
-8) North Carolina
-11) Virginia Tech
-12) New Mexico State
-
-Brendon Desrochers
-2) Kentucky
-4) UCLA
-5) Houston
-5) Iowa
-6) LSU
-
-Chris Donaghy
-3) Tennessee
-5) Houston
-5) Iowa
-5) St. Mary's
-7) Murray State
-
-Noah Garber
-1) Gonzaga
-2) Villanova
-3) Texas Tech
-4) UCLA
-11) Virginia Tech
-
-Chris Godfrey
-1) Arizona
-4) UCLA
-5) Iowa
-7) Michigan State
-7) USC
-
-Harrison Godfrey
-1) Kansas
-2) Duke
-4) Arkansas
-7) Ohio State
-10) Miami (FL)
-
-Patrick Godfrey
-7) Michigan State
-7) Ohio State
-7) USC
-10) San Francisco
-12) Indiana
-
-Kevin Hess
-6) Alabama
-6) Texas
-7) USC
-8) North Carolina
-11) Michigan
-
-Chris Jennings
-2) Auburn
-4) UCLA
-5) Iowa
-6) Alabama
-11) Michigan
-
-JP Kelly
-3) Tennessee
-4) UCLA
-5) Iowa
-6) Alabama
-12) New Mexico State
-
-Nick Kordesch
-2) Villanova
-4) Arkansas
-7) Murray State
-8) San Diego State
-12) Indiana
-
-Anish Kurian
-3) Tennessee
-3) Wisconsin
-5) Iowa
-5) St. Mary's
-12) UAB
-
-Kaleigh Lidstone
-1) Gonzaga
-3) Texas Tech
-4) UCLA
-5) Houston
-13) Vermont
-
-Matt Lidstone
-1) Arizona
-3) Texas Tech
-4) UCLA
-5) Iowa
-11) Virginia Tech
-
-Reed Lyon
-2) Duke
-8) Memphis
-11) Michigan
-11) Virginia Tech
-12) Richmond
-
-Mark McGill
-2) Villanova
-3) Purdue
-4) Illinois
-8) North Carolina
-13) South Dakota State
-
-Justin McNamee
-2) Kentucky
-3) Tennessee
-8) North Carolina
-12) UAB
-13) Vermont
-
-Casey Schutte
-5) Houston
-5) Iowa
-10) Davidson
-11) Michigan
-11) Virginia Tech
-
-Bob Shellenberger
-1) Arizona
-2) Auburn
-4) Arkansas
-11) Virginia Tech
-13) Vermont
-
-Erin Shellenberger
-2) Kentucky
-2) Villanova
-5) Iowa
-6) Alabama
-13) Vermont
-
-Kelly Shellenberger
-2) Villanova
-4) UCLA
-5) St. Mary's
-10) Miami (FL)
-11) Virginia Tech
-
-Scott Sherman
-1) Arizona
-1) Gonzaga
-3) Tennessee
-4) UCLA
-13) South Dakota State
-
-Adam Sidney
-3) Tennessee
-4) Arkansas
-4) UCLA
-5) Iowa
-10) Miami (FL)
-
-Matthew Silva
-4) UCLA
-5) Connecticut
-5) Houston
-5) Iowa
-11) Virginia Tech
-
-Dillon Smith
-4) Illinois
-5) Iowa
-10) Davidson
-12) Indiana
-13) Vermont
-
-Edward Stowe
-3) Texas Tech
-4) UCLA
-5) Houston
-10) Loyola (IL)
-11) Michigan
-
-Geno Tartell III
-1) Arizona
-1) Gonzaga
-4) UCLA
-5) Iowa
-5) Virginia Tech
-
-Dan Vogel
-3) Tennessee
-11) Virginia Tech
-12) Davidson
-12) UAB
-13) South Dakota State
-
-Krysti Walker
-1) Arizona
-3) Tennessee
-4) UCLA
-5) Iowa
-11) Virginia Tech
-
-Ross Warren
-1) Gonzaga
-5) Iowa
-10) San Francisco
-11) Iowa State
-11) Michigan
