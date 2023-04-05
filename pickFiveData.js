ef7f16c6c188dd2c3883a71fe57dcd6462a27e8c
Brendan Bansavage <brendaniscool@gmail.com>
2023-04-04 17:14:07 -0700

    update pick5 data

--- pickFiveData.js
+++ pickFiveData.js
@@ -612,7 +612,7 @@ const pickFiveTeams = [
   { name: "3) Gonzaga", points: 18, round: "Elite 8" },
   { name: "3) Kansas State", points: 18, round: "Elite 8" },
   { name: "3) Xavier", points: 9, round: "Sweet 16" },
-  { name: "4) Connecticut", points: 60, round: "Final" },
+  { name: "4) Connecticut", points: 84, round: "Won Title" },
   { name: "4) Indiana", points: 4, round: "Second Rd" },
   { name: "4) Tennessee", points: 12, round: "Sweet 16" },
   { name: "4) Virginia", points: 0, round: "First Rd" },
@@ -639,13 +639,13 @@ const pickFiveTeams = [
 ];
 
 const pickFivePicks = [
-  { name: "Ben Aparo", score: 137, teams: [0, 34, 36, 41, 47] },
+  { name: "Ben Aparo", score: 161, teams: [0, 34, 36, 41, 47] },
 
   { name: "Brendan Bansavage", score: 59, teams: [33, 40, 44, 12, 13] },
 
   { name: "Zach Baron", score: 23, teams: [31, 38, 43, 51, 59] },
 
-  { name: "Dante Carnevale", score: 60, teams: [36, 45, 13, 14, 18] },
+  { name: "Dante Carnevale", score: 84, teams: [36, 45, 13, 14, 18] },
 
   { name: "Ed Carroll", score: 154, teams: [33, 41, 42, 10, 11] },
 
@@ -663,21 +663,21 @@ const pickFivePicks = [
 
   { name: "Brendon Desrochers", score: 141, teams: [33, 38, 42, 44, 7] },
 
-  { name: "Karl Fils-Aime", score: 99, teams: [0, 36, 44, 55, 15] },
+  { name: "Karl Fils-Aime", score: 123, teams: [0, 36, 44, 55, 15] },
 
-  { name: "Chris Godfrey", score: 123, teams: [30, 36, 44, 46, 56] },
+  { name: "Chris Godfrey", score: 147, teams: [30, 36, 44, 46, 56] },
 
   { name: "Patrick Godfrey", score: 66, teams: [30, 38, 44, 47, 13] },
 
   { name: "Kevin Hess", score: 76, teams: [0, 33, 40, 41, 51] },
 
-  { name: "Chris Jennings", score: 110, teams: [0, 36, 44, 10, 12] },
+  { name: "Chris Jennings", score: 134, teams: [0, 36, 44, 10, 12] },
 
   { name: "John Kelly", score: 32, teams: [34, 35, 43, 51, 12] },
 
   { name: "Keenan Klein", score: 23, teams: [33, 40, 51, 7, 13] },
 
-  { name: "Anish Kurian", score: 101, teams: [36, 40, 44, 11, 13] },
+  { name: "Anish Kurian", score: 125, teams: [36, 40, 44, 11, 13] },
 
   { name: "Reed Lyon", score: 23, teams: [33, 40, 45, 9, 13] },
 
@@ -687,19 +687,19 @@ const pickFivePicks = [
 
   { name: "Casey Schutte", score: 54, teams: [38, 44, 46, 55, 7] },
 
-  { name: "Jeff Selikoff", score: 145, teams: [29, 32, 36, 40, 42] },
+  { name: "Jeff Selikoff", score: 169, teams: [29, 32, 36, 40, 42] },
 
   { name: "Adam Sidney", score: 39, teams: [40, 46, 48, 50, 9] },
 
-  { name: "Josh Silbereis", score: 96, teams: [36, 38, 45, 52, 7] },
+  { name: "Josh Silbereis", score: 120, teams: [36, 38, 45, 52, 7] },
 
   { name: "Josh Smilowitz", score: 89, teams: [33, 40, 44, 48, 56] },
 
-  { name: "Ed Stowe", score: 107, teams: [36, 40, 44, 46, 7] },
+  { name: "Ed Stowe", score: 131, teams: [36, 40, 44, 46, 7] },
 
   { name: "Dan Vogel", score: 64, teams: [31, 37, 38, 44, 46] },
 
-  { name: "Krysti Walker", score: 122, teams: [0, 36, 37, 40, 41] },
+  { name: "Krysti Walker", score: 146, teams: [0, 36, 37, 40, 41] },
 ];
 
 const parseTeamRound = (pickFiveDataset) => {
